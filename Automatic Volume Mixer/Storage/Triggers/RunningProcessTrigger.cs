2f19725352cde146bfeb745f81cce3d485171775
Marcin Szeniak <marc200333@gmail.com>
2017-08-23 19:50:48 +0200

    Added OnStart and OnClose process triggering

--- Automatic Volume Mixer/Storage/Triggers/RunningProcessTrigger.cs
+++ Automatic Volume Mixer/Storage/Triggers/RunningProcessTrigger.cs
@@ -1,18 +1,52 @@
-﻿using System.Linq;
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
 using Avm.Daemon;
 
 namespace Avm.Storage.Triggers
 {
     public class RunningProcessTrigger : NameFilterTriggerBase, ITrigger
     {
+        [Category("Process Trigger")]
+        [Description("Running will fire when any process matching the criteria is running. OnStart will fire only once when the first matching process starts (not on second, third etc.), while OnClose will fire when the last matching process closes.")]
+        [DefaultValue(ProcessTriggerType.Running)]
+        public ProcessTriggerType TriggerType { get; set; }
+
         public override object Clone()
         {
             return MemberwiseClone();
         }
 
+        private bool? _lastState;
+
         public bool ProcessTrigger(object sender, StateUpdateEventArgs args)
         {
-            return Enabled && MixerWatcher.ProcessBuffer.Any(x => MatchName(x.ProcessName));
+            var result = MixerWatcher.ProcessBuffer.Any(x => MatchName(x.ProcessName));
+            try
+            {
+                switch (TriggerType)
+                {
+                    case ProcessTriggerType.Running:
+                        return result;
+                    case ProcessTriggerType.OnStart:
+                        return result && _lastState.HasValue && !_lastState.Value;
+                    case ProcessTriggerType.OnClose:
+                        return !result && _lastState.HasValue && _lastState.Value;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            finally
+            {
+                _lastState = result;
+            }
         }
+    }
+
+    public enum ProcessTriggerType
+    {
+        Running,
+        OnStart,
+        OnClose
     }
 }
